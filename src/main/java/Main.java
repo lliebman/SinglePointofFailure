0151e09168910ddb3d0fe1b87a5043e021beba84
SaraOrlian <orlian.sara@gmail.com>
2020-11-15 14:44:21 -0500

    added fileWriter for output and started putting things in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        ReadFile readFile = new ReadFile(args[0]);
+        readFile.parseFile();
+        //STUFF HAPPENS HERE
+        try {
+            File output = new File("SPFoutput.txt");
+            System.out.println("File created: " + output.getName());
+                FileWriter myWriter = new FileWriter("SPFoutput.txt", false); //overwrite if file exists
+                myWriter.write("Here's what you write"); //INCLUDE STUFF TO BE WRITTEN HERE
+                myWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
