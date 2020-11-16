7d32a95fe5bab5738a6f720d86087d105825d0e0
Tamar <tamartda@gmail.com>
2020-11-16 11:57:13 -0500

    name change

--- src/main/java/NetworkFrame.java
+++ src/main/java/NetworkFrame.java
@@ -2,15 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class NetworkFrame extends JFrame {
-    public NetworkFrame(NodeView view)throws HeadlessException {
+
+    public NetworkFrame(NetworkView view) throws HeadlessException {
         super();
 
-        setSize();
+        setSize(view.getViewSize() + 50, view.getViewSize() + 50);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Network");
         setLayout(new BorderLayout());
 
         add(view, BorderLayout.CENTER);
-
     }
 }
