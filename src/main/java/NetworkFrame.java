dc0917886d8240d4b6f254642dd7600737a266d2
Tamar <tamartda@gmail.com>
2020-11-15 20:55:03 -0500

    fixed size

--- src/main/java/NetworkFrame.java
+++ src/main/java/NetworkFrame.java
@@ -6,7 +6,7 @@ public class NetworkFrame extends JFrame {
     public NetworkFrame(NodeView view) throws HeadlessException {
         super();
 
-        setSize(view.getViewSize() + 50);
+        setSize(view.getViewSize() + 50, view.getViewSize() + 50);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Network");
         setLayout(new BorderLayout());
