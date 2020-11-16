dff5ea8482103f68ffd3c7f94b6bc72285c5badc
Tamar <tamartda@gmail.com>
2020-11-16 12:30:01 -0500

    added main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<GraphNode> list = new ArrayList<>();
+        Graph graph = new Graph(list);
+        NetworkView view = new NetworkView(graph);
+        NetworkFrame frame = new NetworkFrame(view);
+        frame.setVisible(true);
     }
 
 }
