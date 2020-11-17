d38a8df882c811edf439b88172028deea0bfe801
SaraOrlian <orlian.sara@gmail.com>
2020-11-16 19:32:57 -0500

    deleted Readfile.java and put everything in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,60 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        ReadFile readFile = new ReadFile(args[0]);
-        readFile.parseFile();
-        //STUFF HAPPENS HERE
         try {
+            ArrayList<GraphNode> graphNodeList = new ArrayList<>();
+            Graph graph;
+            System.out.println("here 1");
+            File fileInput = new File(args[0]);
+            Scanner input = new Scanner(fileInput);
             File output = new File("SPFoutput.txt");
-            System.out.println("File created: " + output.getName());
-                FileWriter myWriter = new FileWriter("SPFoutput.txt", false); //overwrite if file exists
-                myWriter.write("Here's what you write"); //INCLUDE STUFF TO BE WRITTEN HERE
-                myWriter.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            FileWriter myWriter = new FileWriter(output, false);
+            int counter = 1;
+            System.out.println("here 2");
+            myWriter.write("Network #" + counter);
+            while (input.hasNextLine()) {
+                String string = input.nextLine();
+                if (string.length() == 1) { //line contains a single 0
+                    System.out.println("here 3");
+                    //display SPF
+                    graph = new Graph(graphNodeList);
+                    System.out.println("-----------"+graph.getGraph().toArray().length);
+                    List<GraphNode> pointsOfFailure = graph.getSPF();
+                    for (GraphNode gn : pointsOfFailure) {
+                        System.out.println("I see you");
+                        myWriter.write("\tSPF node " + gn.toString());
+                    }
+                    if(graphNodeList.isEmpty()) {
+                        myWriter.write("\tNo SPF nodes");
+                    }
+                    graphNodeList.clear();
+                    myWriter.write("\n");
+                } else if (string.isBlank()) { //line is blank
+                    System.out.println("here 4");
+                    counter++;
+                    myWriter.write("Network #" + counter);
+                } else if (string.length() > 1){ //line contains a pair of numbers
+                    System.out.println("here 5");
+                    String[] splitBySpace = string.split("\\s+");
+                    GraphNode node1 = new GraphNode(splitBySpace[0]);
+                    GraphNode node2 = new GraphNode(splitBySpace[1]);
+                    node1.addConnection(node2);
+                    node2.addConnection(node1);
+                    System.out.println(node1);
+                    System.out.println(node2);
+                    graphNodeList.add(node1);
+                    graphNodeList.add(node2);
+                }
+            }
+            myWriter.close();
+            System.out.println("here 6");
+        }catch (Exception exception) {
+            exception.printStackTrace();
         }
     }
-
 }
